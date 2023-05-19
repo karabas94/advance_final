ab660c3184b20c1ba462b9a4c3313020593a76e1
karabas94 <alkar_94@ukr.net>
2023-05-19 21:10:45 +0300

    initial commit

--- shop/core/settings.py
+++ shop/core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(4-wts86qfg^o#62u5w9e*c#6@uhzzy_q3^w(t1$lu*&hp))n0'
+SECRET_KEY = os.environ.get('SHOP_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- store/core/settings.py
+++ store/core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l=#&)&16u7tynanb*l_$t6^$2k=5m0vg+=^3^*zv6d!bzbd7&i'
+SECRET_KEY = os.environ.get('STORE_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
