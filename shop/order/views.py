abefc848f3e26893ddf9a352088854ed37c54b2c
karabas94 <112809113+karabas94@users.noreply.github.com>
2023-06-12 02:56:31 +0300

    Update views.py

--- shop/order/views.py
+++ shop/order/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 from cart.cart import Cart
 from order.tasks import send_order_confirmation_email
 from django.core.paginator import Paginator
+from django.contrib import messages
 
 
 @login_required(login_url="/account/login")
